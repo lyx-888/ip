78578790139394dccd0f33c5556e0731897f91ea
lyx-888 <yixuanleong441@gmail.com>
2026-01-25 22:03:52 +0800

    YXBot with mark as done function

--- src/main/java/YXBot.java
+++ src/main/java/YXBot.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class YXBot {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String[] list = new String[100];
+        Task[] list = new Task[100];
         int counter = 0;
 
         System.out.println("____________________________________________________________");
@@ -23,16 +23,47 @@ public class YXBot {
                 }
                 else{
                     System.out.println("____________________________________________________________");
+                    System.out.println("Here are the tasks in your list:");
                     for (int i = 0; i < counter; i++){
-                        System.out.println((i + 1) + ". " + list[i]);
+                        System.out.println((i + 1) + "." + list[i].toString());
                     }
+                    System.out.println("____________________________________________________________");
+                }
+            }
+            else if (input.startsWith("mark ")){
+                int taskNum = Integer.parseInt(input.split(" ")[1]) - 1;
+                if(list[taskNum] == null){
+                    System.out.println("____________________________________________________________");
+                    System.out.println("task does not exist");
+                    System.out.println("____________________________________________________________");
+                }
+                else{
+                    list[taskNum].markAsDone();
+                    System.out.println("____________________________________________________________");
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println(list[taskNum].toString());
+                    System.out.println("____________________________________________________________");
+                }
+            }
+            else if (input.startsWith("unmark ")) {
+                int taskNum = Integer.parseInt(input.split(" ")[1]) - 1;
+                if (list[taskNum] == null) {
+                    System.out.println("____________________________________________________________");
+                    System.out.println("task does not exist");
+                    System.out.println("____________________________________________________________");
+                } else {
+                    list[taskNum].markAsNotDone();
+                    System.out.println("____________________________________________________________");
+                    System.out.println("Ok, I've marked this task as not done yet:");
+                    System.out.println(list[taskNum].toString());
                     System.out.println("____________________________________________________________");
                 }
             }
             else {
                 System.out.println("____________________________________________________________");
                 System.out.println("added: " + input);
-                list[counter] = input;
+                Task t = new Task(input);
+                list[counter] = t;
                 counter++;
                 System.out.println("____________________________________________________________");
             }
