b521b0155f12125d18382f7a914780d1d5219154
lyx-888 <yixuanleong441@gmail.com>
2026-01-25 21:25:03 +0800

    Improved skeletal version of YXBot with list function

--- src/main/java/YXBot.java
+++ src/main/java/YXBot.java
@@ -2,12 +2,13 @@ import java.util.Scanner;
 public class YXBot {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String[] list = new String[100];
+        int counter = 0;
+
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm YXBot");
         System.out.println("What can I do for you?");
-        //System.out.println("____________________________________________________________");
-        //System.out.println("Bye. Hope to see you again soon!");
-        //System.out.println("____________________________________________________________");
+        System.out.println("____________________________________________________________");
         while (true) {
             String input = sc.nextLine();
             if (input.equals("bye")){
@@ -16,11 +17,25 @@ public class YXBot {
                 System.out.println("____________________________________________________________");
                 break;
             }
-            System.out.println("____________________________________________________________");
-            System.out.println(input);
-            System.out.println("____________________________________________________________");
-
-
+            else if (input.equals("list")){
+                if (counter == 0){
+                    System.out.println("Empty list");
+                }
+                else{
+                    System.out.println("____________________________________________________________");
+                    for (int i = 0; i < counter; i++){
+                        System.out.println((i + 1) + ". " + list[i]);
+                    }
+                    System.out.println("____________________________________________________________");
+                }
+            }
+            else {
+                System.out.println("____________________________________________________________");
+                System.out.println("added: " + input);
+                list[counter] = input;
+                counter++;
+                System.out.println("____________________________________________________________");
+            }
         }
         sc.close();
     }
