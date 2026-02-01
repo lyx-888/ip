06df38d25a08b45577b9fbae2606278981b02639
lyx-888 <yixuanleong441@gmail.com>
2026-02-01 21:28:37 +0800

    Add dates and times to YXBot

--- src/main/java/InvalidDeadlineFormatException.java
+++ src/main/java/InvalidDeadlineFormatException.java
@@ -1,6 +1,6 @@
 public class InvalidDeadlineFormatException extends YXBotException{
     public InvalidDeadlineFormatException() {
-        super("Invalid deadline format! Use: deadline [description] /by [time]");
+        super("Invalid deadline format! Use: deadline [description] /by [yyyy-MM-dd HHmm] (e.g., 2023-12-25 1800)");
     }
 
 }
--- src/main/java/InvalidEventFormatException.java
+++ src/main/java/InvalidEventFormatException.java
@@ -1,5 +1,5 @@
 public class InvalidEventFormatException extends YXBotException{
     public InvalidEventFormatException(){
-        super("Invalid event format! Use: event [description] /from [start] /to [end]");
+        super("Invalid event format! Use: event [description] /from [ yyyy-MM-dd HHmm] /to [ yyyy-MM-dd HHmm] (e.g., 2023-12-25 1800)");
     }
 }
